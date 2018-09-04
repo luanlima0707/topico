dfeeeee21dd3fe8f056d3cc95c42af0031bf0fce
Luan Bernardes <luan_poeta@hotmail.com>
2018-09-04 20:05:33 -0300

    Aula jpa 2

--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -2,18 +2,29 @@ package fvs.edu.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+
 public class Aluno implements Serializable{
 
 	
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	private String cpf;
 	private String rg;
 	private Integer matricula;
 	
-	
+	public Aluno() {
+		
+		
+	}
 	
 	
 	public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
