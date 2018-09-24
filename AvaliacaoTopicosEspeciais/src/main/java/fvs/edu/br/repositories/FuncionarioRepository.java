27979b79731e9732ef72cb246556895e43b5cc49
Luan Bernardes <luan_poeta@hotmail.com>
2018-09-24 20:22:48 -0300

    Avaliação

---
+++ AvaliacaoTopicosEspeciais/src/main/java/fvs/edu/br/repositories/FuncionarioRepository.java
@@ -0,0 +1,12 @@
+package fvs.edu.br.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fvs.edu.br.domain.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
+
+	
+}
